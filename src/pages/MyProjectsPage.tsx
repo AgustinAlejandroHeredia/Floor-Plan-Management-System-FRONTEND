3a2572a444e978d2c7be650a5647b24e8331f939
AgustinAlejandroHeredia <102188628+AgustinAlejandroHeredia@users.noreply.github.com>
2026-06-13 17:47:42 -0300

    Update MyProjectsPage.tsx
    hice un cambio y habia quedado === de comparacion y tenia que ser > par que muestre la lista

--- src/pages/MyProjectsPage.tsx
+++ src/pages/MyProjectsPage.tsx
@@ -31,7 +31,7 @@ const MyProjectsPage = () => {
 
                     <p className="comment-text">Total projects {userProjectsList.length}</p>
 
-                    {userProjectsList.length === 0 && (
+                    {userProjectsList.length > 0 && (
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                             {userProjectsList.map((project) => (
                                 <Card
